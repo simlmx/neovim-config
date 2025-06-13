a65ac887847ca9e51f3af1ce3cb759de0002f645
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2025-06-13 03:12:59 +0000

    Make a few colors darker for better legibility

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,6 +3,9 @@ return {
   opts = {
     on_colors = function(colors)
       colors.border = "#565f89"
+      -- Make these darker otherwise they are hard to read.
+      colors.terminal_black = "#828BBF"
+      colors.comment = "#828BBF"
     end,
   },
 }
