e582d49a47736ab5a8da50b074d669fc0702a6b2
Simon Lemieux <simon@reliant.ai>
2025-06-20 16:45:42 -0400

    Tweak colors

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,8 +4,11 @@ return {
     on_colors = function(colors)
       colors.border = "#565f89"
       -- Make these darker otherwise they are hard to read.
+      colors.comment = "#858cce"
       colors.terminal_black = "#828BBF"
-      colors.comment = "#828BBF"
+    end,
+    on_highlights = function(hl, c)
+      hl.ColorColumn = { bg = c.fg_gutter }
     end,
   },
 }
