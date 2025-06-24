0ce95218f0c822dd9716b048a189607425be2683
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2025-06-24 20:32:12 +0000

    Add typescript language server

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,6 +7,7 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        ts_ls = {},
       },
       diagnostics = {
         virtual_text = false,
