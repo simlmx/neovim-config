bc6af7ab0f6d1066b5b9f6202fd20d1c1e718b5c
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2025-06-29 20:58:50 +0000

    Format typescript with both eslint and prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,8 +4,8 @@ return {
     opts = {
       formatters_by_ft = {
         python = { "ruff_fix", "ruff_format" },
-        typescript = { "prettierd", "prettier" },
-        typescriptreact = { "prettierd", "prettier" },
+        typescript = { "prettier", lsp_format = "first" },
+        typescriptreact = { "prettier", lsp_format = "first" },
       },
     },
   },
