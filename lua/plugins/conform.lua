5db27ca98323a55fd138c42e6cbaa813c8669584
Simon Lemieux <simon@reliant.ai>
2025-08-07 13:37:29 -0400

    Use tombi for toml files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,9 +3,10 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        python = { "ruff_fix", "ruff_format" },
+        python = { "ruff_fix", "ruff_format", lsp_format = "last" },
         typescript = { "prettier", lsp_format = "first" },
         typescriptreact = { "prettier", lsp_format = "first" },
+        toml = { "tombi" },
       },
     },
   },
